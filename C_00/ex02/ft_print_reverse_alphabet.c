ce9ae73e9a1a604cd2c0fc551a65b58046c3ba5e
Kadir Kiras <kadir_piece@outlook.com>
2023-02-07 17:22:47 +0300

    c00 ex05

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkiras <42istanbul.com.tr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 19:52:53 by kkiras            #+#    #+#             */
+/*   Updated: 2023/02/02 20:17:36 by kkiras           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
