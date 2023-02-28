38a32a21da0ee57e42c4063ad8ec6aed97086416
kadirkiras <kadirkiras@kadirkirass-MacBook-Pro.local>
2023-02-28 16:50:21 +0300

    42 bitti

--- C/C_03/ex00/ft_strcmp.c
+++ C/C_03/ex00/ft_strcmp.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkiras <42istanbul.com.tr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/17 03:53:52 by kkiras            #+#    #+#             */
-/*   Updated: 2023/02/17 04:12:26 by kkiras           ###   ########.tr       */
+/*   Created: 2023/02/25 11:37:21 by kkiras            #+#    #+#             */
+/*   Updated: 2023/02/25 12:02:27 by kkiras           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	ft_ultimate_div_mod(int *a, int *b)
+int	ft_strcmp(char *s1, char *s2)
 {
-	int	div;
-	int	mod;
+	unsigned int	i;
 
-	div = *a / *b;
-	mod = *a % *b;
-	*a = div;
-	*b = mod;
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
